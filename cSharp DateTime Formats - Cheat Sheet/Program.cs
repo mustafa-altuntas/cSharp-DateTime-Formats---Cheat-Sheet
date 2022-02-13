8a98c90a5a3b674b01726491b288351d9265161c
Mustafa ALTUNTAŞ <2altuntas9@gmail.com>
2022-02-13 16:35:49 +0300

    Add project files.

---
+++ cSharp DateTime Formats - Cheat Sheet/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace cSharp_DateTime_Formats___Cheat_Sheet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt = DateTime.Now;
+            string[] format = {
+               "d", "D",
+               "f", "F",
+               "g", "G",
+               "m",
+               "r",
+               "s",
+               "t", "T",
+               "u", "U",
+               "y",
+               "dddd, MMMM dd yyyy",
+               "ddd, MMM d \"'\"yy",
+               "dddd, MMMM dd",
+               "M/yy",
+               "dd-MM-yy",
+            };
+
+            string date;
+            for (int i = 0; i < format.Length; i++)
+            {
+                date = dt.ToString(format[i], DateTimeFormatInfo.InvariantInfo);
+                Console.WriteLine(String.Concat(format[i], " :", date));
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
